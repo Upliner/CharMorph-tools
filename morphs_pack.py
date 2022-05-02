5f509af07ca434c3a0472c8c4951b28c999e5ceb
Michael Vigovsky <upliner@gmail.com>
2022-05-02 21:36:15 +0300

    Add morph packer

---
+++ morphs_pack.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import os, sys, numpy
+
+verts_cnt = None
+if len(sys.argv)>1:
+    verts_cnt=int(sys.argv[1])
+
+names = bytearray()
+cnt = []
+idx = []
+delta = []
+full = []
+
+
+for file in os.listdir("."):
+    if not file.endswith(".npz") and not file.endswith(".npy"):
+        continue
+    z = numpy.load(file)
+
+    if len(names)>0:
+        names.append(0)
+    names.extend(file[:-4].encode("ascii"))
+
+    if isinstance(z, numpy.ndarray):
+        cnt.append(-1)
+        full.append(z)
+    else:
+        curcnt = len(z["idx"])
+        if verts_cnt and curcnt>=verts_cnt*0.80:
+            result = numpy.zeros((verts_cnt, 3), dtype=numpy.float32)
+            result[z["idx"]] = z["delta"]
+            cnt.append(-1)
+            full.append(result)
+        else:
+            cnt.append(curcnt)
+            idx.extend(z["idx"])
+            delta.extend(z["delta"])
+
+numpy.savez(
+    "__pack__.npz",
+    names=names,
+    cnt=numpy.array(cnt, dtype=numpy.int16),
+    idx=numpy.array(idx, dtype=numpy.uint16),
+    delta=numpy.array(delta, dtype=numpy.float32),
+    full=numpy.array(full, dtype=numpy.float32),
+)
